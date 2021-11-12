1a30f066bfe615393ffe000e0072dfabf1f60842
Maxim Filimonov <max.filimonov@gmail.com>
2021-11-12 12:50:11 +0000

    application update to be able to run

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,5 +1,8 @@
-import firebase from 'firebase';
+import firebase from 'firebase/compat/app';
 import {firebaseConfig} from 'Constants/defaultValues'
+import 'firebase/compat/auth'; //v9
+import 'firebase/compat/database'; //v9
+
 
 firebase.initializeApp(firebaseConfig);
 
