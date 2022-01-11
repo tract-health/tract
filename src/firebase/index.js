bb03485698fdfd8e3f0bcac3b50ce99cea1dcf3f
Maxim Filimonov <max.filimonov@gmail.com>
2022-01-11 13:42:19 +0000

    google analytics through firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,9 +2,12 @@ import firebase from 'firebase/compat/app';
 import {firebaseConfig} from 'Constants/defaultValues'
 import 'firebase/compat/auth'; //v9
 import 'firebase/compat/database'; //v9
+import 'firebase/compat/analytics'; //v9
+import { getAnalytics } from 'firebase/analytics';
 
 
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const auth = firebase.auth();
 const database = firebase.database();
